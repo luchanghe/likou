0ecaba813480c25fa5f136057f5a0e670c97022c
张少杰 <zhangshaojie@xmpaoyou.com>
2021-07-09 14:07:03 +0800

    搜索插入位置

---
+++ 35.搜索插入位置.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 3, 5, 6}
+	n := searchInsert(nums, 5)
+	fmt.Println(n)
+}
+
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		min := left + (right-left)/2
+		if nums[min] == target {
+			return min
+		}
+		if target < nums[min] {
+			right = min - 1
+		} else {
+			left = min + 1
+		}
+	}
+	return left
+}
